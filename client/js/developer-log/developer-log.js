6c041bb44b0a152281dcb41e1e1e83fcec76d12d
Giancarlo <aguecig.9@gmail.com>
2020-05-11 15:25:20 -0400

    Add info to the developer log page

---
+++ client/js/developer-log/developer-log.js
@@ -0,0 +1,61 @@
+const checkCircle = `<i class="fa fa-check-circle" style="color:green;"></i>`;
+const crossCircle = `<i class="fa fa-times-circle" style="color:red;"></i>`;
+
+const bugs = [
+  {
+    date: "22/04/20",
+    resolved: true,
+    description: "After scoring objectives that require spending tactic/strategy tokens, an incorrect ammount of command tokens are given for redistribution during the status phase."
+  },
+  {
+    date: "23/04/20",
+    resolve: false,
+    description: "When a player activates construction primary, then on their next turn tries to move infantry off the planet they built a PDS/Space Dock on, game says the movement is invalid and that only infantry can be transported off the surface of a planet."
+  },
+  {
+    date: "26/04/20",
+    resolved: true,
+    description: "The action card Uprising does not exhaust the targeted player's planet."
+  },
+  {
+    date: "26/04/20",
+    resolved: true,
+    description: "The action card Lucky Shot does not function properly under some instances."
+  },
+  {
+    date: "27/04/20",
+    resolved: true,
+    description: "Movements bug after ground forces are committed to a planet."
+  },
+  {
+    date: "28/04/20",
+    resolved: false,
+    description: "After producing infantry, that same infantry cannot be moved without reloading the game (game does not recognize unit id)."
+  },
+  {
+    date: "28/04/20",
+    resolved: true,
+    description: "Movement bug after committing infantry to Mecatol Rex."
+  },
+  {
+    date: "05/05/20",
+    resolved: false,
+    description: "Faction ability Munitions Reserves will prompt The Barony of Letnev player even if they are not involved in the combat."
+  },
+  {
+    date: "05/10/20",
+    resolved: true,
+    description: "After obtaining the War Sun technology, players cannot produce the War Sun unit during Production."
+  }
+];
+
+bugs.forEach(bug => {
+  let image;
+
+  if (bug.resolved) image = checkCircle;
+  else image = crossCircle;
+
+  $("#bug-log-list").append(`
+    <li>${image} ${bug.date}: ${bug.description}</li>
+    `);
+});
