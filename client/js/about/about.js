dbe1437406c1382decfb666d29037b03f77c3257
aguecig <aguecig.9@gmail.com>
2020-05-18 11:38:02 -0400

    Add some more to the FAQs

--- client/js/about/about.js
+++ client/js/about/about.js
@@ -1,7 +1,11 @@
 const faqs = [
   {
+    question: "Can I play on my mobile device?",
+    answer: "Yes, the app is fully functional on both mobile and desktop devices."
+  },
+  {
     question: "Why did you create this app if one already exists on Table Top Simulator?",
-    answer: "In short: we wanted to be able to automate everything, have no ambiguity in rules implementation, and be able to play both in real time and through correspondence. The app requires no external scripting or downloads. 100% browser based and it does everything for you. All you have to do is play."
+    answer: "In short: we wanted to have a mobile interface, be able to automate everything, have no ambiguity in rules implementation, and be able to play both in real time and through correspondence. The app requires no external scripting or downloads. 100% browser based and it does everything for you. All you have to do is play."
   },
   {
     question: "If I play on the browser, will I be pestered with ads?",
@@ -10,6 +14,10 @@ const faqs = [
   {
     question: "How do I get notifications to show up on my mobile device when it is my turn?",
     answer: "Make sure you are navigating to the site through HTTPS and not HTTP. When you are logged in and it is not your turn, you will recieve a prompt asking you if you want to recieve notifications. Check the box off. Then, add the website to your home screen. You should then recieve notifications."
+  },
+  {
+    question: "Where can I contact you to report an issue, give feedback, etc?",
+    answer: "You can contact us at twanelapp@gmail.com"
   }
 ];
 
